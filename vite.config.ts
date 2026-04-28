55564f92e89fb33fd610eee8c76104051d899e22
XCrasseltX <22590978+XCrasseltX@users.noreply.github.com>
2026-04-28 20:58:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/CrasseltSpaceTech_Page/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
