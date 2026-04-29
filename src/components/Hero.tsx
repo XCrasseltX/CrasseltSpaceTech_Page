0470c143b9fa9e4bff649650f5fc5da2bbb16e9f
XCrasseltX <22590978+XCrasseltX@users.noreply.github.com>
2026-04-29 12:46:18 +0200

    logo auf vergrößern

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              <LogoIcon className="w-64 h-auto"/>
+              <LogoIcon className="w-250 h-auto"/>
             </span>{" "}
             Engeneering
           </h1>{" "}
