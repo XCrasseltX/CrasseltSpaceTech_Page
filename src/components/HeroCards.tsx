e568b9ebf70d04f1add1d3b71c43ef13e47f45c0
XCrasseltX <22590978+XCrasseltX@users.noreply.github.com>
2026-04-29 07:05:17 +0200

    Bug Beheben in HeroCards

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+//import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
